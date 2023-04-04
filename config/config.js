5a49750be0cf3ea528c94a4153f40bcc45961eba
huyme2103 <103888378+huyme2103@users.noreply.github.com>
2023-04-04 08:27:58 +0000

    Pending changes exported from your codespace

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const NodeCache = require('node-cache')
 
 const config = {
     prefix: '/',
-    openaikey: '',
+    openaikey: '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',
     cache: new NodeCache({
         checkperiod: 10000,
         deleteOnExpire: true
